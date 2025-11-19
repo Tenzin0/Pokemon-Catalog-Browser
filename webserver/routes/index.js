4cff6d645dea76a96cce41abd5fe58569b7ecb3f
Tenzin Gaycha <Tgaycha@uci.edu>
2025-11-18 23:28:04 -0800

    added webserver skeleton

---
+++ webserver/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+const URI = 'https://pokeapi.co/api/v2/';
+
+/* GET home page. */
+router.get('/:name', (req, res) => {
+  
+});
+module.exports = router;
---
+++ webserver/routes/pokemon.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+const URI = 'https://pokeapi.co/api/v2/';
+
+router.get('/', (req, res) => {
+    
+})
+module.exports = router
