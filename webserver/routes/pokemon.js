8b1e925bcc3849e67e78e90a840edb8eb7f5b101
Tenzin Gaycha <Tgaycha@uci.edu>
2025-11-19 01:07:44 -0800

    added a getter from pokeAPI

--- webserver/routes/pokemon.js
+++ webserver/routes/pokemon.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 const URI = 'https://pokeapi.co/api/v2/';
+const URL = URI + 'pokemon/'
 
-router.get('/', (req, res) => {
-    
+router.get('/:name', async (req, res) => {
+    const name = req.params.name;
+    const response = await fetch(URL+ name)
+    const data = await response.json();
+
+    res.json(data);
 })
 module.exports = router
