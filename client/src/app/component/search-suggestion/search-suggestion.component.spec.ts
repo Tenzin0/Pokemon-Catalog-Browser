6d7765b26f9c24a3bbd212268aa2908eddb9dce1
Tenzin Gaycha <Tgaycha@uci.edu>
2025-11-18 23:25:17 -0800

    created basic skeleton

---
+++ client/src/app/component/search-suggestion/search-suggestion.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchSuggestionComponent } from './search-suggestion.component';
+
+describe('SearchSuggestionComponent', () => {
+  let component: SearchSuggestionComponent;
+  let fixture: ComponentFixture<SearchSuggestionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SearchSuggestionComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(SearchSuggestionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/component/search-suggestion/search-suggestion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-suggestion',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-suggestion.component.html',
+  styleUrl: './search-suggestion.component.css'
+})
+export class SearchSuggestionComponent {
+
+}
