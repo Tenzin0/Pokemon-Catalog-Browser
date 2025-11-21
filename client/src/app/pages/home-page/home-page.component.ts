b695929828f3a822a56fe15e0dc64eaecda41fa0
Tenzin0 <tenzingaycha1@gmail.com>
2025-11-20 16:30:54 -0800

    added loadAllPokemon() function - to load x pokemon to array

--- client/src/app/pages/home-page/home-page.component.ts
+++ client/src/app/pages/home-page/home-page.component.ts
@@ -13,9 +13,14 @@ import { CardComponent } from '../../component/card/card.component';
 export class HomePageComponent implements OnInit {
 
   constructor(private pokemonService: PokemonService ) {}
-
+  
+  MAX_POKEMON: number = 151;
+  ALL_POKEMON_LIST: any[] = [];
   pokemon: any;
+  isLoadingAll = false;
+
   ngOnInit(): void {
+    this.loadAllPokemon();
   }
   getPokemon(name : string) {
     this.pokemonService.getPokemon(name).subscribe({
@@ -28,4 +33,23 @@ export class HomePageComponent implements OnInit {
       }
     });
   }
+
+  loadAllPokemon() {
+    this.ALL_POKEMON_LIST = [];
+    const MAX = this.MAX_POKEMON;
+
+    for (let id = 1; id <= MAX; id++) {
+      this.pokemonService.getPokemon(id.toString()).subscribe({
+        next: (data) => {
+          this.ALL_POKEMON_LIST.push(data);
+
+          this.ALL_POKEMON_LIST.sort((a, b) => a.id - b.id);
+        },
+        error: (err) => {
+          console.error('Error fetching ID', id, err);
+        }
+      });
+    }
+  }
+
 }
